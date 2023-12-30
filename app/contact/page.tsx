f891c63495312f54c3078bf3cb1088d6792ce1c1
John Montanye <43389272+montanyecreative@users.noreply.github.com>
2023-12-29 18:51:20 -0600

    Updated Contact Us Page Function Name

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/footer";
 import Promotion from "@/components/promotion";
 import ContactForm from "../../components/contactForm";
 
-export default function Resume() {
+export default function ContactUs() {
 	return (
 		<main>
 			<Navbar />
@@ -13,9 +13,7 @@ export default function Resume() {
 				<div className="page-banner-filler bg-black"></div>
 				<div className="container resume-page mx-auto text-white">
 					<h1 className="text-[32px] mt-5">Contact Us</h1>
-					<p className="my-3">
-						Please fill out the form below and we will get in touch with you as soon as we can.
-					</p>
+					<p className="my-3">Please fill out the form below and we will get in touch with you as soon as we can.</p>
 					<ContactForm />
 				</div>
 			</div>
