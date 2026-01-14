c4623313a4a898655abee25d7ead26608cb0734c
John Montanye <montanyecreative@outlook.com>
2026-01-13 18:44:56 -0600

    Copy and image commit

--- components/FeatureGrid.tsx
+++ components/FeatureGrid.tsx
@@ -9,40 +9,45 @@ interface FeatureItem {
 
 const featureItems: FeatureItem[] = [
 	{
+		src: "/website-development/websiteDesign.webp",
+		alt: "Website and App Design",
+		title: "Website Design",
+		description:
+			"We work with you to build a modern and user-friendly website that is optimized for your brand and will work on every device and screen size in a pixel perfect capacity.",
+	},
+	{
+		src: "/website-development/digitalStrategyConsulting.webp",
+		alt: "Site Speed Icon",
+		title: "Digital Strategy",
+		description:
+			"We can help you get past roadblocks or help you generate ideas that will boost your business while maintaining your current vision.",
+	},
+	{
+		src: "/website-development/costReduction.webp",
+		alt: "Site Speed Icon",
+		title: "Cost Reduction",
+		description:
+			"Over the years we've worked out the best ways to reduce costs for websites. Hosting is often free and can be bundled with many tools like web forms, analytics, admin portals, and more for very affordable if not free options.",
+	},
+	{
+		src: "/website-development/seoOptimization.webp",
+		alt: "Site Speed Icon",
+		title: "SEO Optimization",
+		description: "We help review the copy and layout of your website to ensure it is optimized for SEO and Google's latest algorithms.",
+	},
+	{
 		src: "/website-development/sitePerformance.webp",
 		alt: "Site Speed Icon",
 		title: "Website Performance",
-		description: "Description text goes here. This is a placeholder description for the first column.",
+		description:
+			"You can expect Google Core Web Vitals and Lighthouse test scores to be 90% or higher giving you the fastest web experience possible regardless what device or internet connection your user is on.",
 	},
 	{
-		src: "/website-development/sitePerformance.webp",
+		src: "/website-development/toolLeverage.webp",
 		alt: "Site Speed Icon",
-		title: "Title 2",
-		description: "Description text goes here. This is a placeholder description for the second column.",
-	},
-	{
-		src: "/website-development/sitePerformance.webp",
-		alt: "Site Speed Icon",
-		title: "Title 3",
-		description: "Description text goes here. This is a placeholder description for the third column.",
-	},
-	{
-		src: "/website-development/sitePerformance.webp",
-		alt: "Site Speed Icon",
-		title: "Title 4",
-		description: "Description text goes here. This is a placeholder description for the fourth column.",
-	},
-	{
-		src: "/website-development/sitePerformance.webp",
-		alt: "Site Speed Icon",
-		title: "Title 5",
-		description: "Description text goes here. This is a placeholder description for the fifth column.",
-	},
-	{
-		src: "/website-development/sitePerformance.webp",
-		alt: "Site Speed Icon",
-		title: "Title 6",
-		description: "Description text goes here. This is a placeholder description for the sixth column.",
+		title: "Tool Leverage",
+		description:
+			"We have a cutting-edge industry grade knowledge of the latest tools and technologies to help you get the most out of your website and online presence.",
 	},
 ];
 
@@ -51,8 +56,8 @@ export default function FeatureGrid() {
 		<div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12 text-white">
 			{featureItems.map((item, index) => (
 				<div key={index} className="group hover:cursor-pointer text-center">
-					<div className="flex mb-4 justify-center">
-						<img src={item.src} alt={item.alt} width="75" height="75" />
+					<div className="flex mb-4 justify-center w-[75px] h-[75px] mx-auto">
+						<img src={item.src} alt={item.alt} width="75" height="75" className="w-[75px] h-[75px] object-contain" />
 					</div>
 					<h3 className="text-xl font-semibold mb-3 proxima-nova-semibold animated-underline transition-colors">{item.title}</h3>
 					<AnimatedTextLines className="aktiv-grotesk-regular transition-colors">{item.description}</AnimatedTextLines>
